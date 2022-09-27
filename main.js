5604ec80a4a67d86a21cd925f21c1904d21a5722
Rafael Almeida <rafael.almeida.soares2012@gmail.com>
2022-09-26 21:39:56 -0300

    alterar para link direto ao github

--- main.js
+++ main.js
@@ -102,7 +102,7 @@ const projects = [
     imageUrlMobile: 'assets/sicoin.png',
     imageUrlDesktop: 'assets/sicoin.png',
     technologiesPopup: ['PHP', 'MySQL'],
-    technologies: ['PHP', 'MySQL','HTML', 'CSS', 'JavaScript'],
+    technologies: ['PHP', 'MySQL', 'HTML', 'CSS', 'JavaScript'],
     popupImageUrlMobile: 'assets/sicoin.png',
     popupImageUrlDesktop: 'assets/sicoin.png',
     linkLive: '',
@@ -119,7 +119,7 @@ const projects = [
     imageUrlMobile: 'assets/cerbero.png',
     imageUrlDesktop: 'assets/cerbero.png',
     technologiesPopup: ['Python', 'PHP'],
-    technologies: ['Python', 'PHP', 'YOLO', 'MySQL','HTML', 'CSS', 'JavaScript'],
+    technologies: ['Python', 'PHP', 'YOLO', 'MySQL', 'HTML', 'CSS', 'JavaScript'],
     popupImageUrlMobile: 'assets/cerbero.png',
     popupImageUrlDesktop: 'assets/cerbero.png',
     linkLive: '',
@@ -135,7 +135,7 @@ const projects = [
     imageUrlMobile: 'assets/lista-tarefa-spring-r.png',
     imageUrlDesktop: 'assets/lista-tarefa-spring-r.png',
     technologiesPopup: ['Java', 'Spring'],
-    technologies: ['Java', 'Spring', 'Maven', 'PostgreSQL','HTML', 'CSS', 'JavaScript'],
+    technologies: ['Java', 'Spring', 'Maven', 'PostgreSQL', 'HTML', 'CSS', 'JavaScript'],
     popupImageUrlMobile: 'assets/lista-tarefa-spring.png',
     popupImageUrlDesktop: 'assets/lista-tarefa-spring.png',
     linkLive: '',
@@ -166,51 +166,6 @@ function g(e, isClass = false) {
   return document.getElementById(e);
 }
 
-// eslint-disable-next-line no-unused-vars
-function popup(projectIndex) {
-  const project = projects[projectIndex];
-  g('t').innerText = project.name;
-  g('mImage', true).src = project.imageUrlMobile;
-  g('mImageDsk', true).src = project.popupImageUrlDesktop;
-  g('modalText', true).innerText = project.bigDescription;
-  const technologies = project.technologies.map(
-    (t) => `<li class="langu">${t}</li>`,
-  );
-  g('modalTechnologies', true).innerHTML = "<ul>" + unescape(technologies.join('')) + "</ul>"
-  g('modalTextMobile', true).innerText = project.bigDescription;
-  g('modalLinkSource', true).innerHTML = `<button type="button" onclick="javascript:window.open('${project.linkSource}', '_blank');" formtarget="_blank">
-  Visualizar Código<span>
-    <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M17.9989 7.46114C17.9989 6.32124 17.6329 5.25043 16.9342 4.31779C17.2004 3.41969 17.2669 2.27979 17.2004 0.967185C17.1671 0.414508 16.7345 0 16.2022 0C15.9028 0 13.3741 0.0345423 11.9101 1.38169C10.6458 1.1399 9.31495 1.1399 8.01736 1.38169C6.58667 0.0345423 4.05802 0 3.7253 0C3.19296 0 2.76042 0.414508 2.72715 0.967185C2.62734 2.27979 2.72715 3.41969 2.99333 4.31779C2.29462 5.28497 1.92863 6.35579 1.92863 7.46114C1.92863 9.8791 3.7253 12.0553 6.45358 13.0915C6.35377 13.2988 6.28723 13.5406 6.22068 13.7824C3.32604 13.4715 1.9619 10.7427 1.89536 10.639C1.66246 10.1209 1.06357 9.91364 0.56449 10.19C0.0654144 10.4318 -0.134216 11.0535 0.131958 11.5717C0.198501 11.7444 2.02845 15.4404 6.05432 15.8549V18.9637C6.05432 19.5509 6.48686 20 7.05248 20H12.875C13.4406 20 13.8732 19.5509 13.8732 18.9637V14.8532C13.8732 14.2314 13.7401 13.6442 13.5072 13.1261C16.2022 12.0553 17.9989 9.91364 17.9989 7.46114Z"
-        fill="#6070FF" />
-    </svg>
-  </span>
-</button>`;
-
-  g('modalCardsList', true).innerHTML = `<ul class="cards-container">
-<li class="card-detail option">${project.details[0]}</li>
-<li class="card-detail option">
-  <img src="assets/bullets.png" alt="bullet" class="circle" />
-  ${project.details[1]}
-</li>
-<li class="card-detail option">
-  <img src="assets/bullets.png" alt="bullet" class="circle" />
-  ${project.details[2]}
-</li>
-<li class="card-detail optiond2_full">${project.details[0]}</li>
-<li class="card-detail optiond2_full">
-  <img src="assets/bullets.png" alt="bullet" class="circle" />
-  ${project.details[1]}
-</li>
-<li class="card-detail optiond2_full">
-  <img src="assets/bullets.png" alt="bullet" class="circle" />
-  ${project.details[2]}
-</li>
-</ul>`
-  g('myModal').style.display = 'block';
-}
-
 function init() {
   for (let k = 0; k < projects.length; k += 1) {
     const project = projects[k];
@@ -230,7 +185,7 @@ function init() {
         <h4 class="desktop_show">${project.name}</h4>
         <div class="card-detail-box">
           <ul class="cards-container">
-            <li class="card-detail canopy">${project.details[0]}</li>
+            <li class="card-detail option">${project.details[0]}</li>
             <li class="card-detail option">
               <img src="assets/bullets.png" alt="bullet" class="circle" />
               ${project.details[1]}
@@ -249,17 +204,23 @@ function init() {
               ${project.details[2]}
             </li>
           </ul>
-          <p class="instruction">${project.smallDescription}</p>
-          <p class="instruction-show">${project.smallDescription}</p>
+          <p class="instruction">${project.bigDescription}</p>
+          <p class="instruction-show">${project.bigDescription}</p>
           <div class="language1 inline">
             <ul> ${unescape(technologies.join(''))}</ul>
           </div>
         </div>
-        <button type="button" class="btn-one see-project" onclick="popup(${k})">
-          Visualizar
-        </button>
-        
-      </div>
+          <button type="button" class="btn-one see-project" onclick="javascript:window.open('${project.linkSource}', '_blank');" formtarget="_blank">
+            Visualizar Código
+            <span>
+              <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M17.9989 7.46114C17.9989 6.32124 17.6329 5.25043 16.9342 4.31779C17.2004 3.41969 17.2669 2.27979 17.2004 0.967185C17.1671 0.414508 16.7345 0 16.2022 0C15.9028 0 13.3741 0.0345423 11.9101 1.38169C10.6458 1.1399 9.31495 1.1399 8.01736 1.38169C6.58667 0.0345423 4.05802 0 3.7253 0C3.19296 0 2.76042 0.414508 2.72715 0.967185C2.62734 2.27979 2.72715 3.41969 2.99333 4.31779C2.29462 5.28497 1.92863 6.35579 1.92863 7.46114C1.92863 9.8791 3.7253 12.0553 6.45358 13.0915C6.35377 13.2988 6.28723 13.5406 6.22068 13.7824C3.32604 13.4715 1.9619 10.7427 1.89536 10.639C1.66246 10.1209 1.06357 9.91364 0.56449 10.19C0.0654144 10.4318 -0.134216 11.0535 0.131958 11.5717C0.198501 11.7444 2.02845 15.4404 6.05432 15.8549V18.9637C6.05432 19.5509 6.48686 20 7.05248 20H12.875C13.4406 20 13.8732 19.5509 13.8732 18.9637V14.8532C13.8732 14.2314 13.7401 13.6442 13.5072 13.1261C16.2022 12.0553 17.9989 9.91364 17.9989 7.46114Z"
+                  fill="#6070FF" />
+              </svg>
+            </span>
+          </button>
+        </div>
     </div>`;
 
     g('projects').appendChild(template.content.firstChild);
